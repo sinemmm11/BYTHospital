5e599961b28524dbd10f7a2e5bce3ad572778907
sinem <s30227@pjwstk.edu.pl>
2025-12-16 20:42:37 +0100

    Update Employee.cs

--- BYTHospital/Employee.cs
+++ BYTHospital/Employee.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace HospitalSystem
 {
     public abstract class Employee : Person
     {
+       
+        public Department? Department { get; internal set; }
+
         private decimal _salary;
         public decimal Salary
         {
@@ -14,6 +18,26 @@ namespace HospitalSystem
                     throw new ArgumentOutOfRangeException(nameof(Salary), "Salary cannot be negative.");
                 _salary = value;
             }
+        }
+
+       
+        public List<SurgeryStaffParticipation> SurgeryParticipations { get; } = new();
+
+        internal void InternalAddSurgeryParticipation(SurgeryStaffParticipation participation)
+        {
+            if (participation == null)
+                throw new ArgumentNullException(nameof(participation));
+
+            if (!SurgeryParticipations.Contains(participation))
+                SurgeryParticipations.Add(participation);
+        }
+
+        internal void InternalRemoveSurgeryParticipation(SurgeryStaffParticipation participation)
+        {
+            if (participation == null)
+                throw new ArgumentNullException(nameof(participation));
+
+            SurgeryParticipations.Remove(participation);
         }
     }
 }
