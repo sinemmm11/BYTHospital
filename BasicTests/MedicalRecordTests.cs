40ee5a41b5948a7e3a1965b1dfaceb1ece62dda9
sinem <s30227@pjwstk.edu.pl>
2025-12-22 13:25:32 +0100

    Update MedicalRecordTests.cs

--- BasicTests/MedicalRecordTests.cs
+++ BasicTests/MedicalRecordTests.cs
@@ -5,7 +5,6 @@ using System.Linq;
 
 namespace HospitalSystem.Tests
 {
-    // This file specifically tests the MedicalRecord "history hub" logic.
     [TestFixture]
     public class MedicalRecordTests
     {
@@ -19,7 +18,6 @@ namespace HospitalSystem.Tests
             _doctor = new Doctor();
         }
 
-        // Just making sure every patient automatically gets a record when they are created.
         [Test]
         public void PatientCreation_CreatesMedicalRecord()
         {
@@ -27,24 +25,20 @@ namespace HospitalSystem.Tests
             Assert.That(_patient.MedicalRecord.Patient, Is.EqualTo(_patient));
         }
 
-        // This is the big one: verifying that the record correctly sorts different event types by date.
         [Test]
         public void GetAllRecordsChronologically_ReturnsCorrectOrder()
         {
             var now = DateTime.Now;
 
-            // Creating events at different times to check sorting.
-            var diagnosisEvent = new Diagnosis(_patient.MedicalRecord, "Flu", now.AddDays(-1)); // 1 day ago
-            var consultationEvent = new Consultation(_patient.MedicalRecord, now.AddHours(-1), "Checkup"); // 1 hour ago
-            var appointmentEvent = new Appointment(_patient, _doctor, now.AddHours(1)); // 1 hour from now
-            var surgeryEvent = new Surgery(_patient, _doctor, now.AddHours(2)); // 2 hours from now
+            var diagnosisEvent = new Diagnosis(_patient.MedicalRecord, "Flu", now.AddDays(-1)); 
+            var consultationEvent = new Consultation(_patient.MedicalRecord, now.AddHours(-1), "Checkup"); 
+            var appointmentEvent = new Appointment(_patient, _doctor, now.AddHours(1)); 
+            var surgeryEvent = new Surgery(_patient, _doctor, now.AddHours(2)); 
             
-            // Link the surgery to the patient manually since it doesn't use the hub-setter yet.
             _patient.MedicalRecord.Surgeries.Add(surgeryEvent);
 
             var records = _patient.MedicalRecord.GetAllRecordsChronologically().ToList();
 
-            // Confirming the list comes back in the order we expect (oldest to newest).
             Assert.That(records[0], Is.EqualTo(diagnosisEvent));
             Assert.That(records[1], Is.EqualTo(consultationEvent));
             Assert.That(records[2], Is.EqualTo(appointmentEvent));
